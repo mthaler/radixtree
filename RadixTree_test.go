1289ca47a0400a830755b91cebedae742a0bb5db
Michael Thaler <michael.thaler@dlr.de>
2020-10-04 11:36:53 +0200

    Added TestRadixTree_KeysWithPrefix.

--- RadixTree_test.go
+++ RadixTree_test.go
@@ -31,3 +31,18 @@ func TestRadixTree_Keys(t *testing.T) {
 		t.Errorf("r.Keys should be %v", expected)
 	}
 }
+
+func TestRadixTree_KeysWithPrefix(t *testing.T) {
+	r := RadixTree{}
+	r.Put("romane", 1)
+	r.Put("romanus", 2)
+	r.Put("romulus", 3)
+	r.Put("rubens", 4)
+	r.Put("ruber", 5)
+	r.Put("rubicon", 6)
+	r.Put("rubicundus", 7)
+	expected := []string {"romane", "romanus", "romulus", }
+	if !reflect.DeepEqual(r.KeysWithPrefix("rom"), expected) {
+		t.Errorf("r.Keys should be %v", expected)
+	}
+}
