c6a743b3732c187f76da0e7fe530aebd4cd27626
Michael Thaler <michael.thaler@dlr.de>
2020-10-05 12:53:43 +0200

    Added comments to queue.go.

--- queue.go
+++ queue.go
@@ -1,10 +1,14 @@
 package radixtree
 
+// enqueue adds the given element at the end of the queue.
+// It returns the queue with the element added.
 func enqueue(queue []string, element string) []string {
 	queue = append(queue, element)
 	return queue
 }
 
+// dequeue removes the element at the head of the queue
+// It returns the element and the queue with the element removed
 func dequeue(queue []string) (string, []string) {
 	element := queue[0]
 	return element, queue[1:]
